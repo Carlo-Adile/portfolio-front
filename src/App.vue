7d18c0bcad2e9bb494e6bcd51df72b06ecf0f9ef
Carlo-Adile <adile.carlo.a@gmailcom>
2024-05-31 16:33:12 +0200

    milestone 5

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <script>
 import axios from 'axios'
+import ProjectCard from './components/ProjectCard.vue'
 
 export default {
   name: 'App',
+  components: {
+    ProjectCard
+  },
   data() {
     return {
       projects: [],
@@ -20,8 +24,7 @@ export default {
         .get(url)
         .then(response => {
           this.projects = response.data.projects;
-          console.log(response);
-
+          console.log(response.data.projects);
         })
         .catch(err => {
           console.error(err);
@@ -32,19 +35,125 @@ export default {
 </script>
 
 <template>
-  <header>
-    header
+  <header class="container-fluid">
+    <nav class="navbar navbar-expand-sm navbar-light bg-light">
+      <div class="container">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse"
+          data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false"
+          aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="collapsibleNavId">
+          <ul class="navbar-nav me-auto mt-2 mt-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" href="#" aria-current="page">Home
+                <span class="visually-hidden">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-bs-toggle="dropdown"
+                aria-haspopup="true" aria-expanded="false">Dropdown</a>
+              <div class="dropdown-menu" aria-labelledby="dropdownId">
+                <a class="dropdown-item" href="#">Action 1</a>
+                <a class="dropdown-item" href="#">Action 2</a>
+              </div>
+            </li>
+          </ul>
+          <form class="d-flex my-2 my-lg-0">
+            <input class="form-control me-sm-2" type="text" placeholder="Search" />
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
+      </div>
+    </nav>
+
   </header>
+
   <main>
+    <div class="p-5 mb-4 bg-tertiary">
+      <div class="container-fluid py-5">
+        <h1 class="display-5 fw-bold">Custom jumbotron</h1>
+        <p class="col-md-8 fs-4">
+          Using a series of utilities, you can create this jumbotron, just
+          like the one in previous versions of Bootstrap. Check out the
+          examples below for how you can remix and restyle it to your liking.
+        </p>
+        <button class="btn btn-primary btn-lg" type="button">
+          Example button
+        </button>
+      </div>
+    </div>
+
     <div class="container">
       <div class="row">
-        
+        <div class="col-4" v-for="project in projects" :key="project.id">
+          <ProjectCard :project="project" :baseApiUrl="base_api_url" />
+        </div>
       </div>
     </div>
   </main>
+
   <footer>
-    footer
+    <div class="container-fluid p-4 mt-4 bg-light">
+      <div class="row">
+        <div class="col-4">
+          <div class="d-flex gap-3 p-2">
+            <i class="fa-brands fa-linkedin"></i>
+            <i class="fa-brands fa-github"></i>
+          </div>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea fugiat aspernatur quaerat veniam numquam vero
+            maiores tempora animi sapiente nobis. Delectus atque ex praesentium accusantium ducimus at pariatur
+            accusamus a?</p>
+        </div>
+        <div class="col-2">
+          <ul class="list-unstyled">
+            <li class="list-item">
+              <h3>Products</h3>
+            </li>
+            <li class="list-item"><a href="" class="text-decoration-none">some link</a></li>
+            <li class="list-item"><a href="" class="text-decoration-none">some link</a></li>
+            <li class="list-item"><a href="" class="text-decoration-none">some link</a></li>
+            <li class="list-item"><a href="" class="text-decoration-none">some link</a></li>
+          </ul>
+        </div>
+        <div class="col-2">
+          <ul class="list-unstyled">
+            <li class="list-item">
+              <h3>Contacts</h3>
+            </li>
+            <li class="list-item"><a href="" class="text-decoration-none">some link</a></li>
+            <li class="list-item"><a href="" class="text-decoration-none">some link</a></li>
+            <li class="list-item"><a href="" class="text-decoration-none">some link</a></li>
+            <li class="list-item"><a href="" class="text-decoration-none">some link</a></li>
+          </ul>
+        </div>
+        <div class="col-2">
+          <ul class="list-unstyled">
+            <li class="list-item">
+              <h3>Info</h3>
+            </li>
+            <li class="list-item"><a href="" class="text-decoration-none">some link</a></li>
+            <li class="list-item"><a href="" class="text-decoration-none">some link</a></li>
+            <li class="list-item"><a href="" class="text-decoration-none">some link</a></li>
+            <li class="list-item"><a href="" class="text-decoration-none">some link</a></li>
+          </ul>
+        </div>
+        <div class="col-2"></div>
+      </div>
+    </div>
   </footer>
+
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+@import './assets/scss/structure.scss';
+
+i {
+  font-size: 2.5rem;
+}
+</style>
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,33 @@
+<script>
+
+
+export default {
+	name: 'ProjectCard',
+	props: {
+		project: {
+			type: Object,
+			required: true
+		},
+		baseApiUrl: {
+			type: String,
+			required: true
+		}
+	}
+
+}
+</script>
+
+<template>
+	<div class="card mb-3">
+		<!-- <img :src="baseApiUrl + '/storage/' + post.cover_image" alt="{{ project.title }}" class="card-img-top"> -->
+		<div class="card-body">
+			<h3 class="card-title">{{ project.title }}</h3>
+			<p class="card-text line_clamp">{{ project.content }}</p>
+		</div>
+
+
+	</div>
+</template>
+
+
+<style lang="scss"></style>
