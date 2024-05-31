723b71d04e316a18c107c5bd133d75ab4ba9ecbb
Carlo-Adile <adile.carlo.a@gmailcom>
2024-05-31 16:40:02 +0200

    add temporary placehold img to project card

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -19,7 +19,9 @@ export default {
 
 <template>
 	<div class="card mb-3">
-		<!-- <img :src="baseApiUrl + '/storage/' + post.cover_image" alt="{{ project.title }}" class="card-img-top"> -->
+		<img v-if="project.cover_image" :src="baseApiUrl + '/storage/' + project.cover_image" :alt="project.title"
+			class="card-img-top">
+		<img v-else src="https://placehold.co/600x400" :alt="project.title" class="card-img-top">
 		<div class="card-body">
 			<h3 class="card-title">{{ project.title }}</h3>
 			<p class="card-text line_clamp">{{ project.content }}</p>
